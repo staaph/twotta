2d6ce43de9a483b372f69d02946659c164dc2997
staaph <contact.staaph@gmail.com>
2022-04-06 13:05:51 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <Head>
-    <title>Twotta</title>
-    <meta name="description" content="Vue 3 Twitter Clone" />
-  </Head>
-
   <div id="app" class="flex h-screen w-screen">
     <SideNavbar />
     <CreateTweet />
@@ -15,14 +10,12 @@
 import CreateTweet from '@/components/CreateTweet.vue';
 import SideNavbar from '@/components/SideNavbar.vue';
 import TrendSidebar from '@/components/TrendSidebar.vue';
-import { Head } from '@vueuse/head';
 
 export default {
   components: {
     CreateTweet,
     SideNavbar,
     TrendSidebar,
-    Head,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,8 @@ import './index.css';
 
 import App from './App.vue';
 import router from './router';
-import { createHead } from '@vueuse/head';
 
 const app = createApp(App);
-const head = createHead();
-
-app.use(head);
 
 app.use(createPinia());
 app.use(router);
